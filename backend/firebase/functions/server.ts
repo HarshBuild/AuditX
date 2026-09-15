98fb711376e08fdcfcf79eccc75792a55c995c19
HarshChauhanCSE <theharshsinhchauhan@gmail.com>
2026-09-15 15:32:42 +0530

    fix(import): restore .js extension for ES module import

--- backend/firebase/functions/server.ts
+++ backend/firebase/functions/server.ts
@@ -25,7 +25,7 @@ import express, { Request, Response, NextFunction } from 'express'
 import cors from 'cors'
 
 // --- Firebase Admin initialization from env vars ---
-import './firebase-admin-init'
+import './firebase-admin-init.js'
 
 // --- API routers ---
 import scanRouter from './routes/scan.js'
